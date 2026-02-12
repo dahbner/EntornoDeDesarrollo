63669136c21f7c8e18f30909c82087879e57316d
dahbner <dabnerorozco123@gmail.com>
2026-02-12 05:44:42 -0400

    feat: Creating const for presenter.js

--- src/presenter.js
+++ src/presenter.js
@@ -13,3 +13,17 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
+
+const firstM = document.querySelector("#primer-numero-multiplicar");
+const secondM = document.querySelector("#segundo-numero-multiplicar");
+const formM = document.querySelector("#multiplicar-form");
+const divM = document.querySelector("#resultado-multiplicar-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNUmberM = Number.parseInt(firstNUmberM.value);
+  const secondNumberM = Number.parseInt(secondNumberM.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstM, secondM) + "</p>";
+});
